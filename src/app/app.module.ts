574605402f326fba889f397ee48ecebe3ca8cc99
Fernando <fernandodof@gmail.com>
2017-07-15 12:52:06 -0300

    updating modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,28 +5,29 @@ import { HttpModule } from '@angular/http';
 import { Routes, RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AngularmModule } from 'angularm';
+import {
+  HomeComponent,
+  AngularmModule,
+  ListEntitiesComponent,
+  NewEntityComponent,
+  ShowEntityComponent,
+  EditEntityComponent,
+  PageNotFoundComponent
+} from 'angularm';
 
 import { AppComponent } from './app.component';
-import { ListEntityTypeComponent } from './list-entity-type/list-entity-type.component';
-import { NewEntityComponent } from './new-entity/new-entity.component';
-import { ShowEntityComponent } from './show-entity/show-entity.component';
-import { EditEntityComponent } from './edit-entity/edit-entity.component';
 import { EditFormLineComponent } from "app/edit-entity/edit-form-line";
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-  { path: ':entitytypename', component: ListEntityTypeComponent },
+  { path: ':entitytypename', component: ListEntitiesComponent },
   { path: ':entitytypename/new', component: NewEntityComponent },
   { path: ':entitytypename/:key', component: ShowEntityComponent },
-  { path: ':entitytypename/:key/edit', component: EditEntityComponent },
-  { path: '*', component: PageNotFoundComponent }
+  { path: ':entitytypename/:key/edit', component: EditEntityComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListEntityTypeComponent,
     NewEntityComponent,
     ShowEntityComponent,
     EditEntityComponent,
@@ -40,6 +41,7 @@ export const routes: Routes = [
     HttpModule,
     RouterModule.forRoot(routes),
     AngularmModule,
+    BrowserAnimationsModule
 
   ],
   providers: [],
